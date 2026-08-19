003f6cc453805c5adb7c91682ac21a68a3f6196d
Ana Flávia Corrêa <anaflaviacorrea@hotmail.com>
2026-08-18 21:22:45 -0300

    Modificações html e inserção de imagens

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -21,15 +22,7 @@ from core.views import home
 from core import views
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-
-    path('', home),
-      path(
-        'portfolio/',
-        views.portfolio,
-        name="portfolio"
-    ),
-
-
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("portfolio/", views.portfolio, name="portfolio"),
 ]
